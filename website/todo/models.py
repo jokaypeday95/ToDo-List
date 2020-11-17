e0c7f0fbcb0969d6d4f4acf6693f03ec86540d1d
Joko Saputro <jokosaputro616@gmail.com>
2020-11-18 01:37:41 +0700

    membuat models, template, views

--- website/todo/models.py
+++ website/todo/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Todo(models.Model):
-    title = models.TextField(max_length=300)
+    title = models.TextField(max_length=200)
     status = models.BooleanField(default=False)
 
     def __str__(self):
-        return '%s: %s' % (self.id, self.title)
+        return self.title
--- website/todo/views.py
+++ website/todo/views.py
@@ -1,25 +1,48 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, reverse
+from django.http import HttpResponseRedirect
+from django.core.exceptions import ObjectDoesNotExist
+
+from .models import Todo
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'todo/index.html')
+    items = Todo.objects.order_by('-id')
+    return render(request, 'todo/index.html', {'items': items})
 
 def done(request):
-    return render(request, 'todo/index.html')
+    items = Todo.objects.filter(status=True).order_by('-id')
+    return render(request, 'todo/index.html', {'items': items})
 
 def pending(request):
-    return render(request, 'todo/index.html')
+    items = Todo.objects.filter(status=False).order_by('-id')
+    return render(request, 'todo/index.html', {'items': items})
 
 def delete_all(request):
-    return render(request, 'todo/index.html')
+    Todo.objects.all().delete()
+    return HttpResponseRedirect(reverse('index'))
 
 def create(request):
-    return render(request, 'todo/index.html')
+    try:
+        title = request.POST['title']
+        todo = Todo(title=title)
+        todo.save()
+        return HttpResponseRedirect(reverse('index'))
+    except Exception:
+        return HttpResponseRedirect(reverse('index'))    
 
 def update(request, id):
-    return render(request, 'todo/index.html')
-
+    try:
+        todo = Todo.objects.get(id=id)
+        todo.status = not todo.status
+        todo.save()
+        return HttpResponseRedirect(reverse('index'))
+    except ObjectDoesNotExist:
+        return HttpResponseRedirect(reverse('index'))
 def delete(request, id):
-    return render(request, 'todo/index.html')
+    try:
+        todo = Todo.objects.get(id=id)
+        todo.delete()
+        return HttpResponseRedirect(reverse('index'))
+    except ObjectDoesNotExist:
+        return HttpResponseRedirect(reverse('index'))
